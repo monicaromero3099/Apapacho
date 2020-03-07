f57f31c38051b21b6019304762a41ebcb8882c68
monicaromero3099 <43079761+monicaromero3099@users.noreply.github.com>
2020-03-06 20:47:29 -0500

    table1

--- main.js
+++ main.js
@@ -11,3 +11,18 @@ function handleSliderMensageInput() {
 }
 handleSliderMensageInput();
 sliderMensageInput.addEventListener('input', handleSliderMensageInput);
+
+//slider Tabla de propiedades 
+var buttonImage=document.querySelector('especificaciones__image');
+var buttonTableD=document.querySelector('.dimensiones');
+var buttonTableP=document.querySelector('.poliester');
+var buttonTableA=document.querySelector('.algodon');
+var buttonIzq=document.querySelector('.btnIzq');
+var buttonIzq=document.querySelector('.btnDer');
+
+function handleButton(){
+    var index = buttonIzq.
+    buttonImage.setAttribute('src', '/images/peluche' + index + '.png');
+}
+
+buttonImage.addEventListener('click', handleButton);
